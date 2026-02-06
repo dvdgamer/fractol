8c5b523e438dca0b12df6fc979d3f58f0185881e
david <davidnmponte@gmail.com>
2026-02-06 12:36:26 +0100

    fixed seg fault error on display

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "fractol.h"
+/* #include <stdio.h> */
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -26,15 +27,14 @@ int	main()
 	int		offset;
 	void	*mlx;
 	void	*mlx_win;
-	void	*mlx_img;
 	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "fractal-ooooooooo");
-	mlx_img = mlx_new_image(mlx, 1920, 1080);
-	/* img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian); */
-	/* int put_pixel = mlx_pixel_put(mlx, mlx_win, 1000, 500, 2); */
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	int put_pixel = mlx_pixel_put(mlx, mlx_win, 1000, 500, 2);
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx,mlx_win, img.img, 0, 0);
+	mlx_put_image_to_window(mlx,mlx_win, img.img, 10, 10);
 	mlx_loop(mlx);
 }
