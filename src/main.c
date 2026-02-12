70ce84fdd050aac74e89e4927a3e292ee91b8264
david <davidnmponte@gmail.com>
2026-02-12 14:03:03 +0100

    after long work and stress I managed to....put a square on screen :D

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "fractol.h"
-/* #include <stdio.h> */
+#include <stdio.h>
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -22,6 +22,21 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+void	make_square(t_data *data, int x, int y, int size, int color)
+{
+	int	i;
+
+	i = 0;
+	while (i <= size)
+	{
+		my_mlx_pixel_put(data, x + i, y, color);
+		my_mlx_pixel_put(data, x, y + i, color);
+		my_mlx_pixel_put(data, x + size - i, y + size, color);
+		my_mlx_pixel_put(data, x + size, y + size - i, color);
+		i++;
+	}
+}
+
 int	main()
 {
 	int		offset;
@@ -30,11 +45,14 @@ int	main()
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "fractal-ooooooooo");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, 640, 480, "fractal-ooooooooo");
+	img.img = mlx_new_image(mlx, 640, 480);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	int put_pixel = mlx_pixel_put(mlx, mlx_win, 1000, 500, 2);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	/* my_mlx_pixel_put(&img, 5, 22, 0x00FF0000); */
+	make_square(&img, 10, 10, 15, 0x00FF0000);
+	make_square(&img, 200, 200, 25, 0x00FF0000);
+	/* make_square(&img, 27, 27, 200, 0x00FF0000); */
 	mlx_put_image_to_window(mlx,mlx_win, img.img, 10, 10);
 	mlx_loop(mlx);
 }
